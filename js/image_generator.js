9661372a66be8ce1e71d1420a3c524f7b04cc752
Bodymoshref <moshrefandme@gmail.com>
2023-11-21 16:09:56 +0200

    test

---
+++ js/image_generator.js
@@ -0,0 +1,29 @@
+const api = "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"
+const inp = document.getElementById("inp")
+const images = document.querySelector(".images")
+
+const getImage = async () => {
+    const methods = {
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization":`Bearer ${api}`
+        },
+        body:JSON.stringify({
+            "prompt":inp.value,
+            "n":3,
+            "size":"256x256"
+        })
+    }
+    const res = await fetch("https://api.openai.com/v1/images/generations" , methods)
+    const data = await res.json()
+    const listImages = data.data
+    images.innerHTML = ""
+    listImages.map(photo => {
+        const container = document.createElement("div")
+        images.append(container)
+        const img = document.createElement("img")
+        container.append(img)
+        img.src = photo.url
+    })
+}
